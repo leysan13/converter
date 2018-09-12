6b1dcf7f23c893a679e47d9a06703b257a443d4a
Лейсан Шамазова <leisan.shamazova@gmail.com>
2018-09-12 17:16:12 +0300

    Initial commit

---
+++ app/src/main/java/com/leyka1398/onverter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.leyka1398.onverter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
